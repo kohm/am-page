24b3034a9aad51a5db2d1105bb83cb2af8b0b080
lpoveda <lucio.poveda@3sharecorp.com>
2020-02-22 01:24:45 +0100

    adding SEO changes

---
+++ components/content/Card.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Heading from "./Heading";
+import {animated, useSpring} from 'react-spring';
+
+const Card = () => {
+  const props = useSpring({opacity: 1, from: {opacity: 0}});
+  return (
+    <animated.div style={props}>
+      <Heading level={1} text={'Lucio Poveda Bertos'}/>
+      <Heading level={2} text={'ARGENTINEAN INNOVATIVE DEVELOPER LIVING IN SPAIN'}/>
+      <footer>
+        <ul>
+          <li>
+            <a href="https://twitter.com/luciopoveda" title="Twitter" target="_blank">Twitter</a>
+          </li>
+          <li>
+            <a href="https://ar.linkedin.com/in/luciopoveda" title="LinkedIn" target="_blank">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/kohm/" title="GitHub">GitHub</a>
+          </li>
+        </ul>
+      </footer>
+
+      { /*language=SCSS*/}
+      <style jsx>{`
+          ul {
+              display: flex;
+              list-style: none;
+              margin: 0;
+              padding: 0;
+          }
+
+          ul li {
+              margin-right: 16px;
+          }
+
+          a {
+              color: #1b1b1b;
+              display: inline-block;
+              font-family: 'Nunito Sans', sans-serif;
+              font-size: 16px;
+              text-decoration: none;
+          }
+
+          a:active,
+          a:focus,
+          a:visited {
+              color: #1b1b1b;
+              text-decoration: none;
+          }
+
+          a:hover {
+              text-decoration: underline;
+          }
+      `}</style>
+    </animated.div>
+  );
+};
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Heading from "../components/content/Heading";
+import Card from "../components/content/Card";
 
 const Home = () => (
   <div>
@@ -10,10 +11,9 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico"/>
       <link rel="canonical" href="https://luciopoveda.com"/>
       <meta name="description"
-            content="I’m an Spain based Front-end Engineer working mainly with AEM (Adobe Experience Manager) CMS (Content Management System) with 6+ years working as a Front-end developer and 3+ year with AEM"/>
+            content="I’m an Spain based Front-end Engineer working mainly with AEM/React with 8+ years working as a Front-end developer and 3+ year with AEM"/>
       <meta name="msapplication-TileColor" content="#D83434"/>
       <meta name="msapplication-TileImage" content="path/to/tileicon.png"/>
-      <link rel="canonical" href="https://luciopoveda.com"/>
       <link href="https://fonts.googleapis.com/css?family=Baskervville|Nunito+Sans&display=swap" rel="stylesheet"/>
       <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
@@ -22,22 +22,7 @@ const Home = () => (
     </Head>
 
     <main className="card">
-
-      <Heading level={1} text={'Lucio Poveda Bertos'}/>
-      <Heading level={2} text={'ARGENTINEAN INNOVATIVE DEVELOPER LIVING IN SPAIN'}/>
-      <footer>
-        <ul>
-          <li>
-            <a href="https://twitter.com/luciopoveda" title="Twitter" target="_blank">Twitter</a>
-          </li>
-          <li>
-            <a href="https://ar.linkedin.com/in/luciopoveda" title="LinkedIn" target="_blank">LinkedIn</a>
-          </li>
-          <li>
-            <a href="https://github.com/kohm/" title="GitHub">GitHub</a>
-          </li>
-        </ul>
-      </footer>
+      <Card/>
     </main>
 
     { /*language=SCSS*/}
@@ -46,42 +31,12 @@ const Home = () => (
             height: 100%;
         }
 
-        ul {
-            display: flex;
-            list-style: none;
-            margin: 0;
-            padding: 0;
-        }
-
-        ul li {
-            margin-right: 16px;
-        }
-
         main {
             left: 50%;
             position: absolute;
             top: 50%;
             transform: translate(-50%, -50%);
             width: calc(100% - 48px);
-        }
-
-        a {
-            color: #1b1b1b;
-            display: inline-block;
-            font-family: 'Nunito Sans', sans-serif;
-            font-size: 16px;
-            text-decoration: none;
-        }
-
-        a:active,
-        a:focus,
-        a:visited {
-            color: #1b1b1b;
-            text-decoration: none;
-        }
-
-        a:hover {
-            text-decoration: underline;
         }
 
         @media (min-width: 968px) {
